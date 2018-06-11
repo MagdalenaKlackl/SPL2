c8897304831f3b9a6992f639499e07b570238611
Nowiii <171wita19@gmail.com>
2018-06-11 07:06:26 -0700

    Hello

--- uebung3.py
+++ uebung3.py
@@ -1,13 +1,16 @@
 import random
 zahlen = []
-for i in range (0, 10):
-    wurf = random.randint(1,6)
+anzahlWuerfe = input("Wie oft soll Rolette simuliert werden?")
+anzahlWuerfe= int(anzahlWuerfe)
+
+for i in range (0, anzahlWuerfe):
+    wurf = random.randint(1,36)
     zahlen.append(wurf)
-    print(wurf, end="...")
+   # print(wurf, end="...")
 
 print ()
 print ("Ergebnis: ")
-print (zahlen)
+#print (zahlen)
 
 for i in range(1,7):
     print (i,"er :", zahlen.count(i))
